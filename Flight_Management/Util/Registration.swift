a05ae268437ce96bd25d541acfd30d5d6c149524
harsh <harshprnajal17030@gmail.com>
2025-12-29 14:20:19 +0530

    updated user registration process to use optional read and to read correct email and dob

--- Flight_Management/Util/Registration.swift
+++ Flight_Management/Util/Registration.swift
@@ -1,62 +1,92 @@
 import Foundation
 
-func initiateUserRegistration() throws -> Int {
+func initiateUserRegistration() throws(UserError) -> Int {
     let userName = IO.readString(prompt: "Enter user name : ", terminator: " ")
     let password = IO.readString(prompt: "Enter password : ", terminator: " ")
-    let dob = IO.readDate(prompt: "Enter date of birth : ", terminator: " ")
-    let gender = Gender.allCases[IO.readEnumOption(enumType: Gender.self) - 1]
+    let phone = IO.readString(
+        prompt: "Enter phone number : ",
+        terminator: " "
+    )
 
-    let email: String
+    let email: String = readCorrectEmail()
+    let dob = try readCorrectDOB()
 
+    IO.displayEnumOptions(enumType: Gender.self,msg: "Select gender")
+    let gender = Gender.allCases[
+        IO.readEnumOption(enumSize: Gender.allCases.count) - 1
+    ]
+    
+    print("\n")
+    let crewOption: Int? = IO.readOptional(
+        msg: "crew type",
+        readValue: {
+            IO.displayEnumOptions(enumType: CrewType.self, msg: "Select crew type")
+            return IO.readEnumOption(enumSize: CrewType.allCases.count)
+        }
+    )
+    let crewType: CrewType? =
+        crewOption != nil ? CrewType.allCases[crewOption! - 1] : nil
+    
+    let readAddress = {
+        IO.readString(prompt: "Enter address : ", terminator: " ")
+    }
+    let address: String? =
+        crewType != nil
+        ? readAddress()
+        : IO.readOptional(
+            msg: "address",
+            readValue: readAddress
+        )
+
+    guard
+        let newUserId = registerUser(
+            dob: dob,
+            gender: gender,
+            name: userName,
+            email: email,
+            password: password,
+            phone: phone,
+            address: address,
+            crewType: crewType
+        )
+    else {
+        throw UserError.registrationFailed
+    }
+
+    return newUserId
+}
+
+func readCorrectEmail() -> String {
     while true {
         let rawEmail = IO.readString(
-            prompt: "Enter user email : ",
+            prompt: "Enter email : ",
             terminator: " "
         )
 
         let pattern = #"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.com$"#
 
         if rawEmail.range(of: pattern, options: .regularExpression) != nil {
-            email = rawEmail
+            return rawEmail
         } else {
-            print("Wrong email format ‼️\nTry again. \n")
+            print("\nWrong email format ‼️\nTry again. \n")
             continue
         }
-
-        break
     }
-
-    let phone = IO.readString(
-        prompt: "Enter user phone number : ",
-        terminator: " "
-    )
-
-    let crewType: CrewType?
-    let address: String?
-
-    var yesNo = IO.readString(
-        prompt: "Do you want to provide crew type : (y/n)",
-        terminator: " "
-    ).lowercased()
-    if yesNo == "y" {
-        crewType =
-            CrewType.allCases[IO.readEnumOption(enumType: CrewType.self) - 1]
-        address = IO.readString(prompt: "Enter address : ", terminator: " ")
-    } else {
-        crewType = nil
-        
-        yesNo = IO.readString(
-            prompt: "Do you want to provide address : (y/n)",
-            terminator: " "
-        ).lowercased()
-        address =
-            yesNo == "y"
-            ? IO.readString(prompt: "Enter address : ", terminator: " ") : nil
-    }
-    
-    guard let newUserId = registerUser(dob: dob, gender: gender, name: userName, email: email, password: password, phone: phone, address: address, crewType: crewType) else {
-        throw UserError.registrationFailed
-    }
-    
-    return newUserId
 }
+
+func readCorrectDOB() throws(UserError) -> Date {
+    let dob = IO.readDate(prompt: "Enter date of birth : ", terminator: " ")
+    let upperLimit: Date = Calendar.current.date(
+        byAdding: .year,
+        value: -10,
+        to: Date()
+    )!
+
+    if dob >= upperLimit {
+        print("User should be older then 10years.")
+        throw UserError.dobBelowMinimum
+    }
+
+    return dob
+}
+
