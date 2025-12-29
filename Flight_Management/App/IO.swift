1d2eda7c1e62a508c08f0389a89c5475cafdfff1
harsh <harshprnajal17030@gmail.com>
2025-12-29 12:17:54 +0530

    updated IO to read different values and enum options

--- Flight_Management/App/IO.swift
+++ Flight_Management/App/IO.swift
@@ -1,5 +1,8 @@
+import Foundation
+
 struct IO {
-    static func readInt() -> Int {
+    static func readInt(prompt: String, terminator: String = "\n") -> Int {
+        print(prompt, terminator: terminator)
 
         while true {
             if let input = readLine(), let number = Int(input) {
@@ -10,10 +13,11 @@ struct IO {
                 )
             }
         }
-
     }
 
-    static func readDouble() -> Double {
+    static func readDouble(prompt: String, terminator: String = "\n") -> Double
+    {
+        print(prompt, terminator: terminator)
 
         while true {
             if let input = readLine(), let number = Double(input) {
@@ -24,6 +28,89 @@ struct IO {
                 )
             }
         }
+    }
 
+    static func readString(prompt: String, terminator: String = "\n") -> String
+    {
+        print(prompt, terminator: terminator)
+
+        while true {
+            if let input = readLine() {
+                return input
+            } else {
+                print(
+                    "\nSomething wrong with input please try again !!!!\n"
+                )
+            }
+        }
+    }
+
+    static func readDate(prompt: String, terminator: String = "\n") -> Date {
+        print(prompt, terminator: terminator)
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        dateFormatter.locale = Locale(identifier: "hi_IN")
+
+        var input = readString(prompt: "(dd-MM-yyyy)", terminator: " ")
+
+        while true {
+            if let date = dateFormatter.date(from: input) {
+                return date
+            } else {
+                input = readString(
+                    prompt: "Please enter date in dd-MM-yyyy format : ",
+                    terminator: ""
+                )
+            }
+        }
+    }
+
+    static func readEnumOption<T: CaseIterable & CustomStringConvertible>(
+        enumType: T.Type
+    ) -> Int {
+        let menu = enumType.allCases
+
+        for (i, option) in menu.enumerated() {
+            print("\(i+1) \(option.description)")
+        }
+
+        var choice: Int
+
+        while true {
+            choice = IO.readInt(prompt: "Enter the choice : ", terminator: " ")
+
+            if choice <= 0 || choice > menu.count {
+                print(" Wrong choice ‼️ ")
+                print(" Try again \n")
+            } else {
+                return choice
+            }
+        }
+    }
+
+    static func readOptional<T>(
+        prompt: String,
+        terminator: String = "\n",
+        type: T.Type
+    ) -> T? {
+        print(prompt, terminator: terminator)
+
+        print("Y -> Provide value\nN -> No value")
+        let choice = readString(prompt: "Enter your choice : ", terminator: " ").lowercased()
+        if choice == "n" {
+            return nil
+        }
+
+        switch type {
+        case is String.Type:
+            return readString(prompt: "", terminator: "") as? T
+        case is Int.Type:
+            return readInt(prompt: "", terminator: "") as? T
+        case is Double.Type:
+            return readDouble(prompt: "", terminator: "") as? T
+        default:
+            return nil
+        }
     }
 }
